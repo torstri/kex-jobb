ad525da41c52fe930bf693d00854694c6e9a466e
Tor Strimbold <torstri@kth.se>
2023-05-12 15:22:53 +0200

    tester klara

--- feature_selection.py
+++ feature_selection.py
@@ -13,6 +13,8 @@ from mlxtend.plotting import plot_sequential_feature_selection as plot_sfs
 import matplotlib.pyplot as plt
 import json
 
+from sklearn.model_selection import GridSearchCV
+
 
 x = np.load('./dataset/balanced/allFeatures.npy')
 y = np.load('./SIFT/array_data/y.npy')
@@ -22,90 +24,6 @@ def invert_array(indices, total):
     res = np.arange(total)
     return np.delete(res, indices)
 
-# estimator = svm.SVR(kernel="linear", C=100)
-# selector = RFE(estimator, n_features_to_select=10, step=1)
-# selector = selector.fit(x, y)
-# print('support', selector.support_)
-# print('ranking', selector.ranking_)
-
-# selector = VarianceThreshold(0.01)
-# res = selector.fit_transform(x)
-# print(res, " shape: ", res.shape)
-# print('time: ', timer() - now)
-
-def get_important_features(subsets, acc, features):
-    highest_accuracy = acc
-    # Printa iterationer
-    it = 0
-    for subset in subsets:
-        accuracy = subsets[subset]['avg_score']
-        if(highest_accuracy < accuracy):
-            highest_accuracy = accuracy
-            features = subsets[subset]['feature_idx']
-            
-    return features, highest_accuracy
-
-def test_model(model_function):
-    x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.2)  # Too much training data for sfs
-    
-    model = model_function
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
-    accuracy = metrics.accuracy_score(y_test, y_pred) # initial accuracy
-    return accuracy
-    
-def tests(model_function, forwards, num_features, num_tests):
-   
-   
-    old_accuracy = test_model(model_function) # Get initial accuracy
-    
-    accuracy = 0 # max accuracy for feature selection
-    freqs = {k: 0 for k in range(170)} # Dictionary for feature frequency
-    number_of_features = 0
-    # Perform tests
-    i = 1
-    while i <= num_tests:
-        
-        # Perform feature selection
-        sfs_selector = perform_feature_selection(model_function, forwards, num_features)
-        
-        # Get best accuracy and important features
-        features, acc,  = get_important_features(sfs_selector.subsets_,sfs_selector.subsets_[1]['avg_score'], sfs_selector.subsets_[1]['feature_idx'])
-        
-        # Update frequencies
-        for feature in features:
-            freqs[feature] += 1
-        i +=1
-        accuracy += acc # and accuracy
-        number_of_features += len(features)
-
-    accuracy = accuracy / num_tests      
-    number_of_features = number_of_features / num_tests  
-    sorted_freqs = dict(sorted(freqs.items(), key=lambda x:x[1], reverse = True)) # sort frequency list
-    return sorted_freqs, accuracy, old_accuracy, number_of_features
-
-    
-    
-# Perform feature selection
-# @param model_function: ML model
-# @param forwards: SFS if true, SBS else
-# @return sfs_selector: 
-def perform_feature_selection(model_function, forwards, num_features):
-    x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.2)  # Too much training data for sfs
-
-
-    model = model_function
-    sfs_selector = SequentialFeatureSelector(model, k_features=num_features,
-                                             forward=forwards,
-                                             floating=False,
-                                             verbose=2,
-                                             scoring='accuracy',
-                                             cv=3)
-    sfs_selector.fit_transform(x_test, y_test)
-    
-    return sfs_selector
 
 def perform_and_test_feature_selection(model_function):
     now = timer()
@@ -158,52 +76,204 @@ def perform_and_test_feature_selection(model_function):
     print('time: ', timer() - now)
 
 
+# estimator = svm.SVR(kernel="linear", C=100)
+# selector = RFE(estimator, n_features_to_select=10, step=1)
+# selector = selector.fit(x, y)
+# print('support', selector.support_)
+# print('ranking', selector.ranking_)
 
-num_feats = 10
+# selector = VarianceThreshold(0.01)
+# res = selector.fit_transform(x)
+# print(res, " shape: ", res.shape)
+# print('time: ', timer() - now)
+
+def get_important_features(subsets, acc, features):
+    highest_accuracy = acc
+
+    for subset in subsets:
+        print("Subset: ", subset)
+        accuracy = subsets[subset]['avg_score']
+        if(highest_accuracy < accuracy):
+            highest_accuracy = accuracy
+            features = subsets[subset]['feature_idx']
+            
+    return features, highest_accuracy
+
+def test_model(model_function):
+    x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2)  # Too much training data for sfs
+    
+    model = model_function
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    accuracy = metrics.accuracy_score(y_test, y_pred) # initial accuracy
+    return accuracy
+
+# Perform feature selection
+# @param model_function: ML model
+# @param forwards: SFS if true, SBS else
+# @return sfs_selector: 
+def perform_feature_selection(model_function, forwards, num_features):
+    x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2)  # Too much training data for sfs
+
+
+    model = model_function
+    sfs_selector = SequentialFeatureSelector(model, k_features=num_features,
+                                             forward=forwards,
+                                             floating=False,
+                                             verbose=2,
+                                             scoring='accuracy',
+                                             cv=3)
+    sfs_selector.fit_transform(x_test, y_test)
+    
+    return sfs_selector
+
+def param_search():
+    print("")
+
+def tests(model_function, forwards, num_features, num_tests):
+   
+   
+    old_accuracy = test_model(model_function) # Get initial accuracy
+    
+    accuracy = 0 # max accuracy for feature selection
+    freqs = {k: 0 for k in range(170)} # Dictionary for feature frequency
+    number_of_features = 0
+    # Perform tests
+    i = 1
+    index = 1
+    if(not forwards):
+        index = 169
+    while i <= num_tests:
+        print("-------------------------------- Test Nr. ", i, " --------------------------------")
+        # Perform feature selection
+        sfs_selector = perform_feature_selection(model_function, forwards, num_features)
+        
+        # Get best accuracy and important features
+        features, acc  = get_important_features(sfs_selector.subsets_,sfs_selector.subsets_[index]['avg_score'], sfs_selector.subsets_[index]['feature_idx'])
+        
+        # Update frequencies
+        for feature in features:
+            freqs[feature] += 1
+        i +=1
+        accuracy += acc # and accuracy
+        number_of_features += len(features)
+
+    accuracy = accuracy / num_tests      
+    number_of_features = number_of_features / num_tests  
+    sorted_freqs = dict(sorted(freqs.items(), key=lambda x:x[1], reverse = True)) # sort frequency list
+    return sorted_freqs, accuracy, old_accuracy, number_of_features
+
+    
+    
+
+
+
+
+
+
+def perform_tests(forwards, num_features, num_tests):
+    
+    
+        
+    svm_results = {}
+    rf_results = {}
+    knn_results = {}
+    
+    print("-------------------------------- SVM --------------------------------")
+    now = timer()
+    svm_freqs, svm_accuracy, svm_old_accuracy, svm_avg_num_features = tests(svm.SVC(C=100), forwards= forwards, num_features= num_features, num_tests= num_tests)
+    time = timer() - now
+    
+    svm_results["svm_freqs"] = svm_freqs
+    svm_results["svm_accuracy"] = svm_accuracy
+    svm_results["svm_old_accuracy"] = svm_old_accuracy
+    svm_results["svm_avg_num_features"] = svm_avg_num_features
+    svm_results["svm_cum_time"] = time
+    
+    print("-------------------------------- RF --------------------------------")
+    now= timer()
+    rf_freqs, rf_accuracy, rf_old_accuracy, rf_avg_num_features = tests(RandomForestClassifier(n_estimators=5), forwards=forwards, num_features =num_features,num_tests= num_tests)
+    time = timer() - now
+    
+    rf_results["rf_freqs"] = rf_freqs
+    rf_results["rf_accuracy"] = rf_accuracy
+    rf_results["rf_old_accuracy"] = rf_old_accuracy
+    rf_results["rf_avg_num_features"] = rf_avg_num_features
+    rf_results["rf_cum_time"] = time
+    
+    print("-------------------------------- KNN --------------------------------")
+    now= timer()
+    knn_freqs, knn_accuracy, knn_old_accuracy, knn_avg_num_features = tests(KNeighborsClassifier(n_neighbors=8), forwards= forwards, num_features= num_features, num_tests= num_tests)
+    time = timer() - now
+
+    knn_results["knn_freqs"] = knn_freqs
+    knn_results["knn_accuracy"] = knn_accuracy
+    knn_results["knn_old_accuracy"] = knn_old_accuracy
+    knn_results["knn_avg_num_features"] = knn_avg_num_features
+    knn_results["knn_cum_time"] = time
+    
+    # Write to json files
+    filenames = ["svm_SFS.json", "rf_SFS.json", "knn_SFS.json"]
+    if(not forwards):
+        filenames=["svm_SBS.json", "rf_SBS.json", "knn_SBS.json"]
+        
+    fp_svm = open(filenames[0], "w")
+    json.dump(svm_results, fp_svm)
+    fp = open(filenames[1], "w")
+    json.dump(rf_results, fp)
+    fp = open(filenames[2], "w")
+    json.dump(knn_results, fp)
+
+
+num_feats = 167
 num_tests = 2
 
-print("--------- S V C ---------")
-now = timer()
-sorted_freqs, accuracy, old_accuracy, avg_num_features = tests(svm.SVC(C=100), True, num_feats, num_tests)
-time = timer() - now
+perform_tests(False, num_feats,num_tests)
 
-sorted_freqs["avg_features"] = avg_num_features
-sorted_freqs["accuracy"]= accuracy
-sorted_freqs["initiaL_accuracy"] = old_accuracy
-sorted_freqs["cum_time"] = time
-print(sorted_freqs)
-with open("svm.json", "w") as outfile:
-    json.dump(sorted_freqs, outfile)
+# print("--------- S V C ---------")
+# now = timer()
+# sorted_freqs, accuracy, old_accuracy, avg_num_features = tests(svm.SVC(C=100), True, num_feats, num_tests)
+# time = timer() - now
+
+# sorted_freqs["avg_features"] = avg_num_features
+# sorted_freqs["accuracy"]= accuracy
+# sorted_freqs["initiaL_accuracy"] = old_accuracy
+# sorted_freqs["cum_time"] = time
+# print(sorted_freqs)
+# with open("svm.json", "w") as outfile:
+#     json.dump(sorted_freqs, outfile)
 
 
 
 
-# perform_and_test_feature_selection(svm.SVC(C=100))
-# Takes a very long time if n = 100 (default).
-print("\n \n --------- R F ---------")
-now = timer()
-sorted_freqs, accuracy, old_accuracy, avg_num_features = tests(RandomForestClassifier(n_estimators=5), True, num_feats, num_tests)
-sorted_freqs["accuracy"]= accuracy
-sorted_freqs["initiaL_accuracy"] = old_accuracy
-time = timer() - now
-sorted_freqs["cum_time"] = time
-print(sorted_freqs)
-with open("rf.json", "w") as outfile:
-    json.dump(sorted_freqs, outfile)
+# # perform_and_test_feature_selection(svm.SVC(C=100))
+# # Takes a very long time if n = 100 (default).
+# print("\n \n --------- R F ---------")
+# now = timer()
+# sorted_freqs, accuracy, old_accuracy, avg_num_features = tests(RandomForestClassifier(n_estimators=5), True, num_feats, num_tests)
+# sorted_freqs["accuracy"]= accuracy
+# sorted_freqs["initiaL_accuracy"] = old_accuracy
+# time = timer() - now
+# sorted_freqs["cum_time"] = time
+# print(sorted_freqs)
+# with open("rf.json", "w") as outfile:
+#     json.dump(sorted_freqs, outfile)
 
 
 
     
-print("\n \n --------- K N N ---------")
-now = timer()
-sorted_freqs, accuracy, old_accuracy, avg_num_features = tests(KNeighborsClassifier(n_neighbors=8), True, num_feats, num_tests)
-time = timer() - now
-sorted_freqs["accuracy"]= accuracy
-sorted_freqs["initiaL_accuracy"] = old_accuracy
-sorted_freqs["cum_time"] = time
-print(sorted_freqs)
-with open("knn.json", "w") as outfile:
-    json.dump(sorted_freqs, outfile)
+# print("\n \n --------- K N N ---------")
+# now = timer()
+# sorted_freqs, accuracy, old_accuracy, avg_num_features = tests(KNeighborsClassifier(n_neighbors=8), True, num_feats, num_tests)
+# time = timer() - now
+# sorted_freqs["accuracy"]= accuracy
+# sorted_freqs["initiaL_accuracy"] = old_accuracy
+# sorted_freqs["cum_time"] = time
+# print(sorted_freqs)
+# with open("knn.json", "w") as outfile:
+#     json.dump(sorted_freqs, outfile)
     
 
 
